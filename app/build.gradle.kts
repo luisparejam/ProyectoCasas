0d74178ffd51f1b07469e902342adb44d1a31ee3
Luis Pareja <lpareja@hotmail.com>
2026-01-22 22:09:55 -0400

    seleccion y carga de imagen en el formulario

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
 
     implementation(libs.androidx.navigation.compose)
 
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/proyectocasas/ui/pantallas/PantallaFormulario.kt
+++ app/src/main/java/com/example/proyectocasas/ui/pantallas/PantallaFormulario.kt
@@ -1,7 +1,12 @@
 package com.example.proyectocasas.ui.pantallas
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -16,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun PantallaFormulario(navController : NavController){
@@ -23,6 +29,12 @@ fun PantallaFormulario(navController : NavController){
     var nombre by remember {mutableStateOf("")}
 
     var descripcion by remember {mutableStateOf("")}
+
+    var imagenUri by remember { mutableStateOf<Uri?>(null) }
+
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()){
+        uri: Uri? -> imagenUri=uri
+    }
 
     Column(Modifier.padding(16.dp)){
 
@@ -37,6 +49,17 @@ fun PantallaFormulario(navController : NavController){
             isError=nombre.isBlank())
 
         Spacer(Modifier.height(8.dp))
+
+        Button(onClick = {launcher.launch("image/*")}) { Text("Seleccionar imagen")}
+
+        imagenUri?.let{
+            Spacer(Modifier.height(8.dp))
+            Image(
+                painter = rememberAsyncImagePainter(it),
+                contentDescription = "Imagen seleccionada",
+                modifier = Modifier.fillMaxWidth().height(200.dp)
+            )
+        }
 
         OutlinedTextField(
             value = descripcion,
